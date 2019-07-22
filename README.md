d4d7660981330da99b939fe6dd05a2a33cc94ad8
Robert Lee <bobbydwise1@hotmail.com>
2019-07-22 15:32:21 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# Daily Coding Problem 2019-07-21
+# Daily Coding Problem 2019-07-22
 
 ####  _Good morning! Here's your coding interview problem for today.
 
-This problem was asked by Airbnb.
+This problem was asked by Apple.
 
-Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
-
-For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
-
-Follow-up: Can you do this in O(N) time and constant space?_
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds._
--- scripts.js
+++ scripts.js
@@ -1,50 +1,11 @@
 /*
 Good morning! Here's your coding interview problem for today.
 
-This problem was asked by Airbnb.
+This problem was asked by Apple.
 
-Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
-
-For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
-
-Follow-up: Can you do this in O(N) time and constant space?
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 */
 
-const makeRandomNumber = (value) => {
-  return Math.ceil(Math.random()*value);
-}
-
-const randomArray = (length) => {
-  output = [];
-  for ( i = 0; i < length; i++ ) {
-    output.push(makeRandomNumber(10));
-  }
-  return output;
-}
-
-const findLargest = (myArray) => {
-  let first = 0;
-  let firstIndex = 0;
-  let indexArray = [];
-  for ( i = 0; i < myArray.length; i++ ) {
-    indexArray.push(i);
-  };
-    console.log('made it', indexArray)
-  for ( i = 0; i <= myArray.length; i++ ) {
-    if (first <= myArray[i]) {
-      first = myArray[i];
-      firstIndex = i;
-    }
-  };
-  indexArray.splice(firstIndex-1,3)
-  return [first,indexArray];
-};
-
-//let testArray = randomArray(makeRandomNumber(7)+3);
-let testArray = [2,4,6,2,5];
-console.log('testArray = ',testArray)
-let largest = findLargest(testArray);
-console.log('largest = ',largest)
 
 $(document).ready(function() {
   $('#output-section-1').text(1);
