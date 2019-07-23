00038a4670763816ee428cd23492c6659388a9e4
Robert Lee <bobbydwise1@hotmail.com>
2019-07-22 17:18:52 -0700

    Create save point.

--- scripts.js
+++ scripts.js
@@ -6,8 +6,23 @@ This problem was asked by Apple.
 Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 */
 
+//No GUI is assummed.
+
+const randomFunction = () => {
+  console.log('done')
+  //return -1*someValue;
+}
+
+let value = 0;
+let n = 5000;
+
+const jobQueue = (value) => {
+  setTimeout(randomFunction(value), n);
+  console.log('value =',value)
+  return value;
+}
 
 $(document).ready(function() {
-  $('#output-section-1').text(1);
+  $('#output-section-1').text(value);
   $('#output-section-2').text(2);
   });
